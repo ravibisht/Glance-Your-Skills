792595fc7249ca64b323f97fc1e66e3b9300112c
Ravi Bisht <47188858+ravibisht@users.noreply.github.com>
2020-04-26 19:12:21 +0530

    testing

---
+++ GYS-1/src/main/java/com/GYS/app/Gys1Application.java
@@ -0,0 +1,13 @@
+package com.GYS.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gys1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gys1Application.class, args);
+	}
+
+}
---
+++ GYS-1/src/main/java/com/GYS/app/Welcome.java
@@ -0,0 +1,15 @@
+package com.GYS.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Welcome {
+
+	
+	@RequestMapping("/")
+	public String home() {
+		
+		return "index.jsp";
+	}
+}
